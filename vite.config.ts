6368f8a744b990d5b8030db1a205272587059c46
prejby-wq <prejby@gmail.com>
2025-10-20 20:04:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
 const isProd = process.env.BUILD_MODE === 'prod'
 
 export default defineConfig({
-base: '/buge4.veriton.io/',
+base: '/',
   plugins: [
     react(), 
     sourceIdentifierPlugin({
